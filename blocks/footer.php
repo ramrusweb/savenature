1df8ed7510e7b4942383c5b8695de2808351d3dc
rrm1 <rustamramazanovworkmail@gmail.com>
2019-10-08 03:58:12 +0300

    Added: footer

---
+++ blocks/footer.php
@@ -0,0 +1,19 @@
+<footer id="footer" class="footer">
+    <div class="container pt-md-5">
+        <div class="row">
+
+            <div class="col-sm-3 col-md-3">
+                <ul class="footer-menu d-inline-flex m-auto">
+                    <li><a href="/">Главная</a></li>
+                    <li><a href="">Lorem</a></li>
+                    <li><a href="">Lorem</a></li>
+                    <li><a href="">Lorem</a></li>
+                    <li><a href="">Lorem</a></li>
+                    <li><a href="">Lorem</a></li>
+                </ul>
+            </div>
+
+            <hr class="hr border-bottom fixed-bottom">
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -1,17 +1,6 @@
-<!doctype html>
-<html lang="ru">
-
-
-
-
 <?
     $title  = 'SaveNature';
     require 'blocks/head.php';
     require 'blocks/header.php';
     require 'blocks/link_bar.php';
-    ?>
-
-<body>
-
-</body>
-</html>
+    require 'blocks/footer.php';
